9347100e9c01dda5c5737eb48a51472eb0859a76
snagatushar <nagatushars05@gmail.com>
2026-04-12 23:06:47 +0530

    vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import { Toaster } from 'react-hot-toast';
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <Toaster position="top-center" />
       <App />
+      <Analytics />
     </AuthProvider>
   </StrictMode>,
 );
